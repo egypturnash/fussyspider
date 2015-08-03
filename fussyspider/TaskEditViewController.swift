e145714d78130c50b7e1409095ea37b2f80f0ef5
Evan Ostroski <eostroski@gmail.com>
2015-08-03 15:47:21 -0700

    Added base storyboards, seques

---
+++ fussyspider/TaskEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskEditViewController.swift
+//  fussymouse
+//
+//  Created by Evan Ostroski on 7/19/15.
+//  Copyright (c) 2015 Evan Ostroski. All rights reserved.
+//
+
+import UIKit
+
+class TaskEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
